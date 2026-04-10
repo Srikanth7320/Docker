cbea7183af18a4b7b19d3b4ad7652b4b9d0c8927
srikanth <venkatasrikanthkv@gmail.com>
2026-04-10 19:37:01 +0530

    simple user API ui based add users and delete users

--- Docker2/index.js
+++ Docker2/index.js
@@ -4,21 +4,30 @@ const pool = require("./db");
 const app = express();
 app.use(express.json());
 
-// Create table if not exists
-pool.query(`
-  CREATE TABLE IF NOT EXISTS users (
-    id SERIAL PRIMARY KEY,
-    name TEXT
-  );
-`);
+// Create table safely on startup
+(async () => {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users (
+      id SERIAL PRIMARY KEY,
+      name TEXT,
+      age INT,
+      city TEXT
+    );
+  `);
+})();
+
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
 
 // Create user
 app.post("/users", async (req, res) => {
-  const { name } = req.body;
+  const { name, age, city } = req.body;
+
   const result = await pool.query(
-    "INSERT INTO users(name) VALUES($1) RETURNING *",
-    [name]
+    "INSERT INTO users(name, age, city) VALUES($1, $2, $3) RETURNING *",
+    [name, age, city]
   );
+
   res.json(result.rows[0]);
 });
 
@@ -34,4 +43,13 @@ app.get("/", (req, res) => {
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
+});
+
+//Delete User
+app.delete("/users/:id", async (req, res) => {
+  const { id } = req.params;
+
+  await pool.query("DELETE FROM users WHERE id = $1", [id]);
+
+  res.json({ message: "User deleted" });
 });
